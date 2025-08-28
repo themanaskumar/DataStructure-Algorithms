a840c9a55fb9dd09d1e77d0a8837f58218c88f32
Manas Kumar <themanaskumar1@gmail.com>
2025-08-28 23:24:55 +0530

    Best time to buy and sell stocks

---
+++ BuySellStock.java
@@ -0,0 +1,43 @@
+// Best Time to Buy and Sell Stock (Leetcode #121)
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+// Example 1:
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+// Example 2:
+// Input: prices = [7,6,4,3,1]
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+
+import java.util.*;
+
+public class BuySellStock {
+    int getMaxProfit(int[] prices){
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        for(int i = 1; i < prices.length; i++){
+            int profit = prices[i]-minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+            minPrice = Math.min(minPrice, prices[0]);
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the array elements: ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        BuySellStock solver = new BuySellStock();
+        int result = solver.getMaxProfit(arr);
+        System.out.println("Maximum profit = "+result);
+    }
+}
